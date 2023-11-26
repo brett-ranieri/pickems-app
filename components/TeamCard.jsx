0a1944a6c7f4847866802813b49203db1000e9a8
Brett Ranieri <brett.ranieri@gmail.com>
2023-11-26 00:12:20 -0700

    initial styling/confirm no state delay
    designed three render options based on the value of isPicked state, testing with styling proved that there are no delay issues with state...lag shown in logs likely due to how I was logging state on console

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -2,29 +2,24 @@ import React, { useState, useEffect } from "react";
 
 export const TeamCard = ({ team, clicked, game, picks }) => {
 	const [isPicked, setIsPicked] = useState(null);
+
 	// if you pass `picks` to this component, it will know if it is picked, or if its opponent is picked, because it will have `game`
 	// logic for that is like -
 	// if this team's game exists in the picks array, check if the team id matches or not, set `isPicked` to true or false
 	// else if it doesn't exist in the picks array, set `isPicked` to null
 	// then you can use that state to determine the styling
 
-	// tried adding second function to onClick
-	// thought was maybe running checkPick onClick would update faster than useEffect
-	// did not work as hoped, still seeing delay on logs
-	// const clicking = async () => {
-	// 	clicked(team.id, game.id);
-	// 	checkPick();
-	// };
+	// turns out delayed issue was with my console logs, not with state setting
+	// was able to tell once I added styling!! :)
 
 	const checkPick = async () => {
 		if (picks.filter((pick) => pick.id === game.id).length > 0) {
 			const pick = picks.filter((pick) => pick.id === game.id);
 			const choosenTeam = pick[0].choosenTeam;
+			// optional chaining used on team throughout component
 			if (choosenTeam === team?.id) {
-				console.log("winner");
 				setIsPicked(true);
 			} else {
-				console.log("loser");
 				setIsPicked(false);
 			}
 		} else {
@@ -34,15 +29,51 @@ export const TeamCard = ({ team, clicked, game, picks }) => {
 
 	useEffect(() => {
 		checkPick();
-		console.log(team?.name, isPicked);
 	}, [picks]);
 
-	// optional chaining on `teams` and `thisTeam` in case it doesn't exist yet when component tries to initially render
-	// let thisTeam = teams?.find((t) => t.id === teamId);
-	// console.log(thisTeam);
-	return <div onClick={() => clicked(team.id, game.id)}>{team?.name}</div>;
+	// is there a better way to specify the return when isPicked is null????
+
+	if (isPicked === null) {
+		return (
+			<>
+				<div
+					className='w-80 h-10 mt-4 font-bold flex items-center justify-center rounded hover:cursor-pointer bg-gray-200'
+					// seemed easier to do outside of tailwind, found solution here:
+					// https://stackoverflow.com/questions/70903204/tailwindcss-custom-background-color-not-working
+					style={{ color: `#${team?.color}` }}
+					onClick={() => clicked(team.id, game.id)}
+				>
+					{team?.name}
+				</div>
+			</>
+		);
+	}
+
+	return (
+		<>
+			{isPicked ? (
+				<div
+					className='w-80 h-10 mt-4 font-bold flex items-center justify-center rounded hover:cursor-pointer border-black border-slate-600 border-4'
+					// seemed easier to do outside of tailwind, found solution here:
+					// https://stackoverflow.com/questions/70903204/tailwindcss-custom-background-color-not-working
+					style={{ backgroundColor: `#${team?.color}`, color: `#${team?.alt_color}` }}
+					onClick={() => clicked(team.id, game.id)}
+				>
+					{team?.name}
+				</div>
+			) : (
+				<div
+					className='w-80 h-10 mt-4 font-bold flex items-center justify-center rounded hover:cursor-pointer bg-gray-200 text-slate-600'
+					onClick={() => clicked(team.id, game.id)}
+				>
+					{team?.name}
+				</div>
+			)}
+		</>
+	);
 };
 
+// ENDED UP DOING OUTSIDE OF TAILWIND...
 // tailwind color syntax: declare outside of the style prop
 // const bgColor = `bg-[#${team.color}]`
 // in the div: style={`${bgColor} mt-2`}
