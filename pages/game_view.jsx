d6137c1e0762eb9fbb5c24e3f86f35444cecdde2
Brett Ranieri <brett.ranieri@gmail.com>
2023-11-29 18:07:58 -0700

    create picks endpoint/write getPick and setPrevPicks functions
    get all picks in endpoint, then in game_view filter all picks based on current user, if picks exist load them, if not then display original view

--- pages/game_view.jsx
+++ pages/game_view.jsx
@@ -8,11 +8,8 @@ const GameViewPage = () => {
 	const [isSubmitted, setIsSubmitted] = useState(false);
 
 	const getGames = async () => {
-		console.log("hello??");
-		// if you're not changing a variable, _always_ declare as a `const`
 		const results = await fetch(`http://localhost:3000/api/games`);
 		const upcomingGames = await results.json();
-		console.log(upcomingGames);
 		setGames(upcomingGames);
 	};
 
@@ -22,13 +19,34 @@ const GameViewPage = () => {
 		setTeams(teams);
 	};
 
+	const getPicks = async () => {
+		const results = await fetch(`http://localhost:3000/api/picks`);
+		const prevPicks = await results.json();
+		console.log("PP: ", prevPicks);
+		setPrevPicks(prevPicks);
+	};
+
+	const setPrevPicks = async (prevPicks) => {
+		// manually setting user below
+		const user = 2;
+		const userPicks = prevPicks.filter((pick) => {
+			return pick.user_id === user;
+		});
+		if (userPicks.length) {
+			console.log(user);
+			console.log(userPicks);
+			setPicks(userPicks);
+			setIsSubmitted(true);
+		} else {
+			console.log("no picks yet");
+		}
+	};
+
 	useEffect(() => {
-		console.log("running useeffect");
 		getTeams();
 		getGames();
+		getPicks();
 	}, []);
-
-	console.log(games);
 
 	const clicked = async (id, gameId) => {
 		const pick = {
@@ -36,41 +54,25 @@ const GameViewPage = () => {
 			chosen_team: id,
 			game_id: gameId,
 		};
-		console.log(pick);
-
 		const tempPicks = picks?.filter((pick) => pick.game_id !== gameId);
 		setPicks([...tempPicks, pick]);
 	};
 
 	const handleSubmit = async () => {
+		// should this actually be at the end of the fetch? only changing if submit was successful?
 		setIsSubmitted(true);
-		console.log("submitted");
-
 		// post request to endpoint, body is stringified picks
 		const postPicksRes = await fetch(`http://localhost:3000/api/submit-picks`, {
 			method: "POST",
+			//by stringifying here, don't need to in the endpoint
 			body: JSON.stringify(picks),
 		});
-		// in the endpoint, you'll be able to log req.method as 'POST' and req.body will be your data
-		// because its already JSON and you're not parsing it, instead of the JSON.stringify(data) like the example
-		// you should be able to just put `req.body` in the parameters array after the query
-		// (if that doesn't work try parsing it and stringifying it again im maybe 90% confident)
-
-		// you probably want to use a json_populate_recordset in your query
-		// to post all the picks at once (see PR write up for example of query)
-		// when you set up your picks object in `clicked`, make the keys the same as the table column headers
-
-		// when you're successful you'll be able to console.log(await postPicksRes.json())
-		// and it'll be the array of picks you just posted because of the returning * in the query
-		// assuming you do a res.send at the end of the post method in the endpoint
 		console.log(await postPicksRes.json());
 	};
-	console.log(isSubmitted);
 
 	return (
 		<>
 			<p className='text-3xl font-bold mb-4'>This is the game view page</p>
-			{/* // didn't need to declare this map outside the default return, can map right here inside {} */}
 			{games.map((game) => (
 				<div
 					key={game.id}
