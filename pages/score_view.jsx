408e492901920c2991a09d91601545175c44e674
Brett Ranieri <brett.ranieri@gmail.com>
2023-12-06 00:13:48 -0700

    set current user, render their name and picks
    write `setUserInfo` function, then render data that has been stored in `user` and `picks` state.
    
    has to be a better way to do this, needed to add another fetch inside `setUserInfo` because I was trying to filter `allPicks` array before it is set. I think I need to make this function wait until `allPicks` is filled
    
    also solved an undefined rendering issue with some conditional rendering

--- pages/score_view.jsx
+++ pages/score_view.jsx
@@ -4,7 +4,9 @@ import { ScoreCard } from "../components/ScoreCard";
 const ScoreViewPage = () => {
 	const [games, setGames] = useState([]);
 	const [picks, setPicks] = useState([]);
-	const [user, setUser] = useState();
+	// switched to null instead of () thinking it might
+	// help with undefined name issue...it did not
+	const [user, setUser] = useState(null);
 	const [allPicks, setAllPicks] = useState([]);
 	let allScores = [];
 
@@ -34,16 +36,52 @@ const ScoreViewPage = () => {
 		setAllPicks(allPicks);
 	};
 
+	const setUserInfo = async () => {
+		console.log("called");
+		// can manually change userId here
+		const userId = 1;
+		const activeUser = users.filter((user) => {
+			return user.id === userId;
+		});
+		console.log(activeUser);
+		setUser(activeUser);
+
+		// needed to add fetch below to fix an undefined issue...but doesnt feel right
+		// there has to be a way to use the allPicks state here and
+		// not running a second fetch to the api...
+
+		// had same code as getUserScore and was trying to filter
+		// allPicks state, but no matter where I tried to call setUserInfo
+		// allPicks was just empty array when called, only way I have found to
+		// consistently populate so far is this...but there must be another way
+		// it works for getUserScore consistently so I know I'm missing
+		// something...
+		const results = await fetch(`http://localhost:3000/api/picks`);
+		const allPicks = await results.json();
+
+		// is it bad coding practice to copy/paste this bit here
+		// feels like it, but also think I need the function below
+		// to run repeatedly in the loop, so keeping seperate to not
+		// overlap and/or break something
+		// console.log("1: ", allPicks);
+		const userPicks = await allPicks.filter((pick) => {
+			return pick.user_id === userId;
+		});
+		console.log(userPicks);
+		setPicks(userPicks);
+	};
+
 	// re-factored all previous functions to all run in a loop
 	const getUserScore = async (user) => {
 		let tally = [];
+		// console.log("2: ", allPicks);
 		const userPicks = allPicks.filter((pick) => {
 			return pick.user_id === user.id;
 		});
 		const checkForWinner = async (pick) => {
 			const gameMatch = async (game) => {
 				if (game.id === pick.game_id) {
-					// log allows me to check that scores are accurate
+					// log below allows me to check that scores are accurate
 					// console.log(game.id, game.winner, pick.chosen_team);
 					if (game.winner === pick.chosen_team) {
 						tally.push(1);
@@ -69,17 +107,20 @@ const ScoreViewPage = () => {
 			const score = 0;
 			allScores.push({ user: user.id, name: user.name, score: score });
 		}
-
 		// console.log(user.id, tally);
 	};
-
 	users.forEach(getUserScore);
+
+	// setUserInfo(); - calling here creates infinite loop...
 	// console.log(allPicks);
 	// console.log(allScores);
+	console.log("User: ", user);
+	console.log("Picks: ", picks);
 
 	useEffect(() => {
 		getGames();
 		getAllPicks();
+		setUserInfo();
 	}, []);
 
 	return (
@@ -98,6 +139,18 @@ const ScoreViewPage = () => {
 					/>
 				</div>
 			))}
+			<div>
+				{/* had a tough time with rendering the username...
+        was returning undefined and chased a bunch of different ways
+        to call functions and define variables before thinking of
+        having it wait for user to return true before rendering */}
+				{user ? <p>Active User: {user[0].name}</p> : null}
+				{picks.map((pick) => (
+					<div key={pick.chosen_team}>
+						{pick.game_id}, {pick.chosen_team}
+					</div>
+				))}
+			</div>
 		</>
 	);
 };
