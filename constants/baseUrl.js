e58401e8ccabf548fbd4f2a43edd83aaf6dc4ae1
Allison Hill <50920539+AllisonSHill@users.noreply.github.com>
2024-01-12 16:43:23 -0700

    try manually setting it

--- constants/baseUrl.js
+++ constants/baseUrl.js
@@ -2,7 +2,7 @@ const baseUrl =
   process.env.VERCEL_ENV === 'production'
     ? `https://${process.env.VERCEL_URL}`
     : process.env.VERCEL_ENV === 'preview'
-    ? `https://${process.env.VERCEL_BRANCH_URL}`
+    ? `https://pickems-app-git-working-3-brett-ranieri.vercel.app/`
     : 'http://localhost:3000'
 
 export default baseUrl
