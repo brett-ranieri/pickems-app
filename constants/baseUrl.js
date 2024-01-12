129b025367190e0b071a0fa1fc5c93ba194d1084
Allison Hill <50920539+AllisonSHill@users.noreply.github.com>
2024-01-12 16:39:44 -0700

    update baseUrl values

--- constants/baseUrl.js
+++ constants/baseUrl.js
@@ -1,6 +1,11 @@
 const baseUrl =
-  process.env.VERCEL_ENV === 'production' || process.env.VERCEL_ENV === 'preview'
+  process.env.VERCEL_ENV === 'production'
     ? `https://${process.env.VERCEL_URL}`
+    : process.env.VERCEL_ENV === 'preview'
+    ? `https://${process.env.VERCEL_BRANCH_URL}`
     : 'http://localhost:3000'
 
 export default baseUrl
+
+
+// https://vercel.com/docs/projects/environment-variables/system-environment-variables
