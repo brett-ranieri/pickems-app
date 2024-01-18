fc22647bf529e520a6249dd21a26bafecaa2e4a4
Brett Ranieri <brett.ranieri@gmail.com>
2024-01-18 11:53:18 -0700

    Update users.jsx

--- constants/users.jsx
+++ constants/users.jsx
@@ -1,15 +1,16 @@
 // test Users table
 const users = [
 	{ id: 1, name: "Allison" },
-	{ id: 2, name: "Brett" },
-	{ id: 3, name: "Maurice" },
-	{ id: 4, name: "Biers" },
-	{ id: 5, name: "Benny" },
-	{ id: 6, name: "Flo" },
-	{ id: 7, name: "Ferdinand" },
-	{ id: 8, name: "Taylor" },
-	{ id: 9, name: "Travis" },
-	{ id: 10, name: "Donna" },
+	{ id: 2, name: "Andrew" },
+	{ id: 3, name: "Brett" },
+	{ id: 4, name: "Dave" },
+	{ id: 5, name: "Hannah" },
+	{ id: 6, name: "Ian" },
+	{ id: 7, name: "Kyle" },
+	{ id: 8, name: "Lucas" },
+	{ id: 9, name: "Ron" },
+	{ id: 10, name: "Tim" },
+	{ id: 11, name: "Travis" },
 ];
 
 export default users;
