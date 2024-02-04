24e4499193730ea93bb60594e477b1a40b296f0e
Brett Ranieri <brett.ranieri@gmail.com>
2024-02-03 23:29:24 -0700

    add stat results

---
+++ constants/stats-results.js
@@ -0,0 +1,13 @@
+const stat_results = [
+	{
+		week: 3,
+		stats: [
+			{ team_id: "33", points: 10, yards: 336 },
+			{ team_id: "25", points: 34, yards: 413 },
+			{ team_id: "8", points: 31, yards: 442 },
+			{ team_id: "12", points: 17, yards: 319 },
+		],
+	},
+];
+
+export default stat_results;
