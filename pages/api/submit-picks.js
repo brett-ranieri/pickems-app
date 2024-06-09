21d9feada4401027452119fc24efeb130555bddf
Brett Ranieri <brett.ranieri@gmail.com>
2024-06-08 22:22:53 -0600

    carry over stat-picks changes to picks endpoint
    NEED TO COME BACK AND RE-FACTOR

--- pages/api/submit-picks.js
+++ pages/api/submit-picks.js
@@ -34,16 +34,51 @@ export default async function submitPicks(req, res) {
 			// moved return up into this if statement because it wants results.rows
 			res.json(results.rows);
 		} else if (req.method === "PUT") {
+			console.log("game putting");
 			const updatedPicks = JSON.parse(req.body);
-			// is it bad form to parse here and not stringify again before query?
-			updatedPicks.forEach(async function (updatedPick) {
-				console.log(updatedPick);
-				results = await client.query(
-					`update public.picks set chosen_team = '${updatedPick.chosen_team}' where game_id = '${updatedPick.game_id}' and user_id = '${updatedPick.user_id}' returning *`
-				);
-			});
-			// seperate return statement here because this wants just results
-			res.json(results);
+			console.log(updatedPicks);
+			const gameIdsToDelete = updatedPicks.map((x) => x.game_id);
+			console.log(gameIdsToDelete);
+			const paramCount = updatedPicks.map((_, index) => "$" + (index + 2)).join(",");
+			console.log(paramCount);
+			await client.query(
+				`
+				delete from public.picks where user_id=$1 and game_id in (${paramCount})
+				`,
+				[updatedPicks[0].user_id, ...gameIdsToDelete]
+			);
+			console.log("game deleted");
+			results = await client.query(
+				`
+			  INSERT INTO public.picks
+			    (
+			      user_id,
+			      chosen_team,
+			      game_id,
+						week
+			    )
+			  SELECT
+			      user_id,
+			      chosen_team,
+			      game_id,
+						week
+			  FROM
+			      json_populate_recordset(null::public.picks, $1)
+			  RETURNING *
+			  `,
+				[req.body]
+			);
+			console.log(results.rows);
+			res.json(results.rows);
+			// // is it bad form to parse here and not stringify again before query?
+			// updatedPicks.forEach(async function (updatedPick) {
+			// 	console.log(updatedPick);
+			// 	results = await client.query(
+			// 		`update public.picks set chosen_team = '${updatedPick.chosen_team}' where game_id = '${updatedPick.game_id}' and user_id = '${updatedPick.user_id}' returning *`
+			// 	);
+			// });
+			// // seperate return statement here because this wants just results
+			// res.json(results);
 		}
 	} catch (err) {
 		console.log(err);
