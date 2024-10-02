faa1e666a059cfc43e55811f2d802e9dcf8d6225
Brett Ranieri <brett.ranieri@gmail.com>
2024-10-02 15:27:35 -0600

    conditional rendering for PickWeekView
    display message stating no picks if nothing submitted

--- components/PickWeekView.jsx
+++ components/PickWeekView.jsx
@@ -20,19 +20,23 @@ export const PickWeekView = ({ user, week, teams, formattedPicks }) => {
 		<>
 			<div className='bg-lime-300 bg-opacity-70 m-4 p-1 rounded'>
 				<p className='text-3xl text-lime-800 font-black underline m-4'>Week {week} Picks:</p>
-				<div className='flex flex-col justify-around items-center mb-6'>
-					{picksToMap?.map((pick) => (
-						<div
-							key={pick.game_id}
-							className='text-lg'
-						>
-							<TeamCard
-								team={teams?.find((x) => x.id === pick.chosen_team)}
-								history={true}
-							/>
-						</div>
-					))}
-				</div>
+				{!picksToMap.length ? (
+					<p className='text-lg m-4'>No picks submitted for this week</p>
+				) : (
+					<div className='flex flex-col justify-around items-center mb-6'>
+						{picksToMap?.map((pick) => (
+							<div
+								key={pick.game_id}
+								className='text-lg'
+							>
+								<TeamCard
+									team={teams?.find((x) => x.id === pick.chosen_team)}
+									history={true}
+								/>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 
 			<div className='mt-80'>.</div>
